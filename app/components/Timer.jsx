643a826c02afe7476660f6c71d87124132b372e6
Hoaveth <hoaveth.gaming@gmail.com>
2023-03-07 15:14:37 +0800

    Added the countdown timer functionality

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,5 +1,17 @@
-export default function Timer() {
-  const options = ["Pomodoro", "Short Break", "Long Break"];
+export default function Timer({
+  stage,
+  switchStage,
+  seconds,
+  minutes,
+  toggleTimer,
+  runTimer,
+}) {
+  const options = [
+    { name: "Pomodoro", value: 25 },
+    { name: "Short Break", value: 5 },
+    { name: "Long Break", value: 10 },
+  ];
+
   return (
     <div className=" w-10/12 mx-auto text-white flex flex-col justify-center items-center mt-10">
       <div className="flex gap-5 ">
@@ -8,19 +20,25 @@ export default function Timer() {
             <h1
               key={index}
               className={`${
-                index === 0 ? "bg-gray-500 bg-opacity-30 " : ""
-              }rounded-lg p-1 p-2 cursor-pointer transition-all`}
+                index === stage ? "bg-gray-500 bg-opacity-30 " : ""
+              } rounded-lg p-1 p-2 cursor-pointer transition-all`}
+              onClick={() => switchStage(index)}
             >
-              {option}
+              {option.name}
             </h1>
           );
         })}
       </div>
       <div className="mt-10 mb-10">
-        <h1 className="text-9xl font-bold select-none m-0">25:00</h1>
+        <h1 className="text-9xl font-bold select-none m-0">
+          {minutes}:{seconds}
+        </h1>
       </div>
-      <button className="px-16 py-2 text-3xl rounded-md bg-white text-blue-500 uppercase font-bold">
-        Start
+      <button
+        className="px-16 py-2 text-3xl rounded-md bg-white text-blue-500 uppercase font-bold"
+        onClick={() => toggleTimer()}
+      >
+        {runTimer ? "Stop" : "Start"}
       </button>
     </div>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,76 @@
+"use client";
+
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
 import Navbar from "./components/Navbar";
 import Timer from "./components/Timer";
 import About from "./components/About";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [pomodoro, setPomodoro] = useState(25);
+  const [shortBreak, setShortBreak] = useState(5);
+  const [longBreak, setLongBreak] = useState(10);
+  const [stage, setStage] = useState(0);
+  const [countDown, setCountDown] = useState(60 * pomodoro);
+  const [runTimer, setRunTimer] = useState(false);
+
+  useEffect(() => {
+    let timer;
+    const selectedStage = setTimer(stage);
+    if (runTimer) {
+      setCountDown(60 * selectedStage);
+      timer = setInterval(() => {
+        setCountDown((countDown) => countDown - 1);
+      }, 1000);
+    } else {
+      clearInterval(timer);
+    }
+    return () => {
+      clearInterval(timer);
+    };
+  }, [runTimer]);
+
+  // useEffect(() => {
+  //   if (countDown < 0 && runTimer) {
+  //     setRunTimer(false);
+  //     setCountDown(0);
+  //   }
+  // }, [countDown, runTimer]);
+
+  const switchStage = (index) => {
+    setRunTimer(false);
+    setStage(index);
+    setCountDown(60 * setTimer(index));
+  };
+
+  const setTimer = (index) => {
+    let selectedStage =
+      index === 0 ? pomodoro : index === 1 ? shortBreak : longBreak;
+    return selectedStage;
+  };
+
+  const seconds = String(countDown % 60).padStart(2, 0);
+  const minutes = String(Math.floor(countDown / 60)).padStart(2, 0);
+
+  const toggleTimer = () => setRunTimer((t) => !t);
+
   return (
     <div className="bg-gray-900 min-h-screen">
       <div className="max-w-4xl min-h-screen mx-auto">
         <Navbar />
-        <Timer />
+        <Timer
+          stage={stage}
+          switchStage={switchStage}
+          countDown={countDown}
+          toggleTimer={toggleTimer}
+          seconds={seconds}
+          minutes={minutes}
+          runTimer={runTimer}
+        />
         <About />
       </div>
     </div>
